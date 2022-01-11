bfbb2d96cf6623a7a06d7faf309b9446d40bf73c
Salina913 <94294010+Salina913@users.noreply.github.com>
2022-01-11 13:50:26 +0530

    Add files via upload

---
+++ remainder.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Main function related information
+"""
+
+def get_remainder(num, den):
+    ''' athis function calculates the remainder of num/den'''
+    
+    rem=num % den
+    print(rem)
+    
+get_remainder(18, 6)
+get_remainder(19,3)
